1e70154cfff363c9406ca2d50e6c82c0ffbe67eb
garrulous abyss <2945138243@qq.com>
2018-04-04 17:08:29 +0200

    AC了
    穷举就能通过的，为什么要用动态规划？

--- 1007. Maximum Subsequence Sum (25).cpp
+++ 1007. Maximum Subsequence Sum (25).cpp
@@ -1,4 +1,4 @@
-//8个测试点，只对了4个，还有一个“段错误”，不明白什么意思，，坑先放在这里，之后填
+
 //题目链接：https://www.patest.cn/contests/pat-a-practise/1007
 
 #include <iostream>
@@ -7,69 +7,38 @@
 using namespace std;
 
 int main()
+
 {
     int K;
     cin>>K;
     int in[K];
-    int index_i=INT_MIN;
-    int index_j=INT_MIN;
+    int sum = 0;
+    int max_sum=INT_MIN;
+    int index_i, index_j;
+    index_i=index_j=-1;
+
     for(int i=0;i<K;i++){
         cin>> in[i];
     }
-
-    cerr<<"in[K] is :";
-    for(int i=0;i<K;i++){
-        cerr<<in[i]<<" ";
-    }
-
-
-    int matrix[K][K];
-    for(int i=0;i<K;i++){
-        for(int j=0;j<K;j++)
-            matrix[i][j]=0; //initialization
-    }
-    int max_num= INT_MIN;
-    bool first_time=true;
-
-    for(int i=K-1;i>=0;i--){
-        for(int j=0;j<=i;j++){
-            if(i==K-1)
-                matrix[i][j]= in[j];//last row
-            else
-                matrix[i][j]= matrix[i+1][j]+in[j+K-i-1];
-
-            if(matrix[i][j]>=max_num){
-                if(first_time){
-                    cerr<<"point #1"<<endl;
-                    max_num= matrix[i][j];
-                    index_i=i;
+    
+    for(int i=0;i<K;i++)
+    {
+        sum=0;
+      for(int j=i;j<K;j++)
+        {
+          sum +=in[j];
+            if(sum>max_sum){
+              index_i=i;
                     index_j=j;
-                    first_time=false;
-                    cerr<<"max_num: "<<max_num<<endl;
+                    max_sum=sum;
                 }
-                else if(i<=index_i || j <=index_j){
-                    cerr<<"point #2"<<endl;
-                    max_num= matrix[i][j];
-                    index_i=i;
-                    index_j=j;
-                    cerr<<"max_num: "<<max_num<<endl;
-                }
-            }
         }
     }
-
-    for(int i=0;i<K;i++){
-        cerr<<endl;
-        for(int j=0;j<K;j++)
-            cerr<<matrix[i][j]<<" "; //initialization
-    }
-
-
-    if (max_num>0)
-        cout<<max_num<<" "<<in[index_j]<<" "<< in[index_j+K-index_i-1];
+  
+    if(max_sum<0)
+      cout<<0<<" "<<in[0]<<" "<<in[K-1];
     else
-        cout<<0<<" "<<in[0]<<" "<<in[K-1];
-
+      cout<<max_sum<<" "<<in[index_i]<<" "<<in[index_j];
 
     return 0;
 }
