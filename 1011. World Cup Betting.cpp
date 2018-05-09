dfadaaf03dab2432d419ffe5bdc2eb24b1523264
garrulous abyss <2945138243@qq.com>
2018-05-09 02:11:32 +0200

    十分钟内一次AC

---
+++ 1011. World Cup Betting.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    float a[3][3];
+    for(int i=0;i<3;i++){
+        cin>>a[i][0]>>a[i][1]>>a[i][2];
+    }
+    float sum=1.0;
+
+    for(int i=0;i<3;i++){
+        if(a[i][0]>a[i][1] && a[i][0]>a[i][2]){
+            cout<<"W ";
+            sum *= a[i][0];
+            continue;
+        }
+        else if(a[i][1]>a[i][0] && a[i][1]>a[i][2]){
+            cout<<"T ";
+            sum *= a[i][1];
+            continue;
+        }
+        else{
+            cout<<"L ";
+            sum *= a[i][2];
+            continue;
+        }
+    }
+    printf("%.2f",(sum*0.65-1)*2);
+    return 0;
+}
